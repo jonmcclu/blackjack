aaa1d984ae24527541eb53600210c06449a94c79
Jonas McClure <jonmcclu@developernexus.app>
2021-09-24 10:31:27 -0400

    Including logging lines for main, classes, and app.

--- app/app.py
+++ app/app.py
@@ -15,3 +15,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S',
+                    level=logging.INFO)
